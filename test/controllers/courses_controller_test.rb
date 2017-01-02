cae1406469b2e42741f364b2a6b601c28c142fd6
RongtongJin <794220751@qq.com>
2017-01-02 14:41:46 +0800

    2017/1/2

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
-class CoursesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
+class CoursesControllerTest < ActionDispatch::IntegrationTest
+  # def setup
+  #   @user       = users(:michael)
+  #   @user2      =users(:rongtongjin)
+  #   @teacher    = teacher(:teacherluo)
+  #   @course1 = courses(:one)
+  #
+  # end
+  #
+  # test "should get selected by teacher not student" do
+  #
+  # end
+  #
+  # test "should get chart by teacher not student" do
+  #
+  # end
+  #
+  # test "should get open" do
+  # end
+  #
+  # test "should get close" do
+  #
   # end
 end
