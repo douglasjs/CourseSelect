e05806afd220d219eacd42ec63e8722be3192395
RongtongJin <794220751@qq.com>
2017-01-03 11:04:52 +0800

    reverse merge qiaocode

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -13,50 +13,19 @@ module CoursesHelper
     }
     param[week_data] + 1
   end
-  #生成11行7列的数据
-  def get_current_curriculum_table(courses,user)
-    # course_time = Array.new(11) { Array.new(7, '') }
-    course_time = Array.new(11) {Array.new(7) {Array.new(3, '')}}
+  def get_current_curriculum_table(courses)
+    course_time = Array.new(11) { Array.new(7, '') }
     courses.each do |cur|
-      real_course_name = cur.name
-      @grades = cur.grades
-      # check whether the course is open.
-      @grades.each do |grade|
-        if grade.user.name == user.name
-          if grade.open==true
-            # if it is open, append "_open" to the course name.
-           real_course_name = real_course_name.concat("_open")
-          end
-        end
-      end
       cur_time = String(cur.course_time)
-      cur_id = cur.course_time
-      end_j = cur_time.index('(')#index第一次出现的字节位置 end_j=2
+      end_j = cur_time.index('(')
       j = week_data_to_num(cur_time[0...end_j])
       t = cur_time[end_j + 1...cur_time.index(')')].split("-")
       for i in (t[0].to_i..t[1].to_i).each
-        course_time[(i-1)*7/7][j-1][0] = real_course_name
-        course_time[(i-1)*7/7][j-1][1] = cur.course_week
-        course_time[(i-1)*7/7][j-1][2] = cur.class_room
+        course_time[(i-1)*7/7][j-1] = cur.name
       end
     end
     course_time
   end
-
-  def is_open_course(course, user)
-    @grades=course.grades
-    is_open = false
-    @grades.each do |grade|
-      if grade.user.name == user.name
-        if grade.open == true
-          is_open = true
-          break
-        end
-      end
-    end
-    is_open
-  end
-
   def get_course_info(courses, type)
     res = Set.new
     courses.each do |course|
@@ -64,7 +33,6 @@ module CoursesHelper
     end
     res.to_a.sort
   end
-
   def check_course_condition(course, key, value)
     if value == '' or course[key] == value
       return true
